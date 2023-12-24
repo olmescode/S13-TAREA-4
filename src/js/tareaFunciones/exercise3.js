cfa7422ffad73ff46e2730f888e654cd7dd11a9e
olmescode <gdavidgaleas@outlook.com>
2023-12-23 20:19:18 -0500

    refactor: update the code to use the method EsNumeroPrimo from utils

--- src/js/tareaFunciones/exercise3.js
+++ src/js/tareaFunciones/exercise3.js
@@ -5,20 +5,17 @@ por 1 y por sí mismo.
 Parameters:
 numero: El número que se quiere verificar como primo.
 */
-const esNumeroPrimo = (numero) => {
-    numero = parseInt(numero);
+import { EsNumeroPrimo } from '../utils.js'
+//const { EsNumeroPrimo } = require('../utils');
 
-    if (numero <= 1) {
-        return numero + " No es un número primo.";
+const verificarSiEsPrimo = (numero) => {
+    const esPrimo = EsNumeroPrimo(numero);
+    
+    if (esPrimo) {
+        return `${numero} es un número primo.`;
+    } else {
+        return `${numero} no es un número primo.`;
     }
-
-    for (let i = 2; i < numero; i++) {
-        if (numero % i === 0) {
-            return numero + " No es un número primo.";
-        }
-    }
-
-    return numero + " ¿es un número primo? Sí";
 };
 
-export {esNumeroPrimo}
+export {verificarSiEsPrimo}
